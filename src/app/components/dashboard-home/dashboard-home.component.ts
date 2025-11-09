0b191b5ee0655668e7fc106dcbb70f744f2e2e26
Juliana <Jramirez939@cue.edu.co>
2025-11-09 16:49:36 -0500

    inicio del administrador completado

--- src/app/components/dashboard-home/dashboard-home.component.ts
+++ src/app/components/dashboard-home/dashboard-home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FondoComponent } from '../fondo-imagen/fondo.component';
 
 @Component({
   selector: 'app-dashboard-home',
-  imports: [],
+  standalone: true,
+  imports: [FondoComponent],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
